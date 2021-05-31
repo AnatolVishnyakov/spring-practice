97ed30df17066f35956fda157927f1f3bfc5859b
Anatol V <malfenius@gmail.com>
2021-05-31 22:46:09 +0300

    Custom log config

--- custom-log/src/main/java/ru/vish/log/db/LoggerConfig.java
+++ custom-log/src/main/java/ru/vish/log/db/LoggerConfig.java
@@ -1,4 +1,13 @@
 package ru.vish.log.db;
 
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
 public class LoggerConfig {
+    @PostConstruct
+    public void init() {
+
+    }
 }
